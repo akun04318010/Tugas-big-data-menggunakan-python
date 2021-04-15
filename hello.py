0454a05a5712ee27b35352e89ee153dff96f7f39
akun04318010 <82586407+akun04318010@users.noreply.github.com>
2021-04-15 19:49:07 +0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+print ("======================================")
+print ("   Biodata Sederhana Dengan Python   ")
+print ("======================================")
+ 
+nama = input("Masukkan nama: ")
+kelas = input("Masukkan kelas: ")
+kampus = input("Inputkan kampus: ")
+ 
+print ("nama, Saya %s, kelas saya %s kampus saya %s kampus" % (nama, kelas, kampus))
